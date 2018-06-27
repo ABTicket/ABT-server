2be3ae4787dfb810e56b8e997eee36e22b4082b3
liaozelin <489531912@qq.com>
2018-06-27 21:09:36 +0800

    后台执行

--- build.sh
+++ build.sh
@@ -9,4 +9,5 @@ sudo docker cp . ticket-server:/app
 sudo docker start ticket-server
 
 sudo docker exec ticket-server /bin/bash -c \
-    "cd src && go build main.go && ./main >out.log 2>&1 &"
+    "cd src && go build main.go && nohub ./main >out.log 2>&1 &"
+echo "Deploy done!"
